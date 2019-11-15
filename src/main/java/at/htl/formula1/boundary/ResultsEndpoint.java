89ca740b0497bc53d7b8c6d0aab262f6ba18ba95
tealeon <leon.tea2811@gmail.com>
2019-11-15 16:20:16 +0100

    added findWinnerOfRace

--- src/main/java/at/htl/formula1/boundary/ResultsEndpoint.java
+++ src/main/java/at/htl/formula1/boundary/ResultsEndpoint.java
@@ -1,6 +1,8 @@
 package at.htl.formula1.boundary;
 
 import at.htl.formula1.entity.Driver;
+import at.htl.formula1.entity.Race;
+import at.htl.formula1.entity.Result;
 
 import javax.json.Json;
 import javax.json.JsonObject;
@@ -24,22 +26,40 @@ public class ResultsEndpoint {
      */
     @GET
     @Path("points")
+    @Produces(MediaType.APPLICATION_JSON)
     public JsonObject getPointsSumOfDriver(
             @QueryParam("name") String name
     ) {
-        TypedQuery<Driver> query = em
-                .createNamedQuery("Driver.findDriverPointsByName", Driver.class)
+        TypedQuery<Driver> query1 = em
+                .createNamedQuery("Driver.findDriverIdByName", Driver.class)
                 .setParameter("NAME", name);
-        Driver d = query.getSingleResult();
-        return (JsonObject) d;
+        Driver d = query1.getSingleResult();
+        Long driverId = d.getId();
+        TypedQuery<Result> query2 = em
+                .createNamedQuery("Result.findDriverPointsById", Result.class)
+                .setParameter("DRIVER", driverId);
+        Result r = query2.getSingleResult();
+
+        return (JsonObject) r;
     }
 
     /**
      * @param id des Rennens
      * @return
      */
-    public Response findWinnerOfRace(long id) {
-        return null;
+    @GET
+    @Path("winner/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String findWinnerOfRace(@PathParam("id") long id) {
+        TypedQuery<Race> query = em.createNamedQuery("Race.findRaceById", Race.class)
+                .setParameter("ID", id);
+        Race r = query.getSingleResult();
+        Long raceId=r.getId();
+
+        TypedQuery<String> query2 = em.createNamedQuery("Result.findWinnerById", String.class)
+                .setParameter("RACE", raceId);
+        String winner = query2.getSingleResult();
+        return winner;
     }
 
 
