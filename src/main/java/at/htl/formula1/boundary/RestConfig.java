8579ba7478498556840f7d93b6a5056b3cfe92c1
Leon Tea <leon.tea@hotmail.com>
2019-11-20 21:32:22 +0100

    Verbesserung: RestConfig added

---
+++ src/main/java/at/htl/formula1/boundary/RestConfig.java
@@ -0,0 +1,9 @@
+package at.htl.formula1.boundary;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class RestConfig extends Application {
+}
