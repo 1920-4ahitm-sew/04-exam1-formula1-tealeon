8810019600792cea91dfb56055f6b1cd92dad76a
Leon Tea <leon.tea@hotmail.com>
2019-11-21 15:22:27 +0100

    Verbesserung: added NamedQuery

--- src/main/java/at/htl/formula1/entity/Team.java
+++ src/main/java/at/htl/formula1/entity/Team.java
@@ -9,6 +9,10 @@ import javax.persistence.*;
  */
 @Entity
 @Table(name="F1_TEAM")
+@NamedQuery(
+        name = "Team.findByName",
+        query = "select t from Team t where t.name = :NAME"
+)
 public class Team {
 
     @Id
